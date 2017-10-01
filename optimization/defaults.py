a92d64dbbce26bff176a6d65e470b01f35f892b7
Tarcisio Fischer <tarcisio.fischer.cco@gmail.com>
2017-10-01 12:44:14 -0300

    Use armijo backtracking as the default line search algorithm

--- optimization/defaults.py
+++ optimization/defaults.py
@@ -1,12 +1,12 @@
 from ._common import default_grad_f_approx
 from ._direction import steepest_descent
-from ._line_search import equal_interval_search
+from ._line_search import armijo_backtracking_line_search
 from ._stop_criterea import default_stop_criterea
 
 DEFAULT_STRATEGY_FUNCTIONS = {
     'grad_f': default_grad_f_approx,
     'stop_criterea': default_stop_criterea,
     'compute_direction': steepest_descent,
-    'compute_step': equal_interval_search,
+    'compute_step': armijo_backtracking_line_search,
     'plot_helper': lambda *args, **kwargs: None,
 }
