8b1fd3259a92e8e6ca33695f6250051ffc96a727
Tarcisio Fischer <tarcisio.fischer.cco@gmail.com>
2017-10-03 01:23:20 -0300

    Add adjoint method for objective function derivative

--- examples/lagrangian_problems/mechanical_project/unconstrained_mechanical_project_optimization.py
+++ examples/lagrangian_problems/mechanical_project/unconstrained_mechanical_project_optimization.py
@@ -1,7 +1,7 @@
 import functools
 
 from examples.lagrangian_problems.mechanical_project.constants import P_y_4, h_ini
-from examples.lagrangian_problems.mechanical_project.linear_system import u, dudx
+from examples.lagrangian_problems.mechanical_project.linear_system import u, dudx, dfdx_adjoint
 from optimization import armijo_backtracking_line_search, default_stop_criterea
 from optimization.defaults import DEFAULT_STRATEGY_FUNCTIONS
 from optimization.minimize import minimize
@@ -9,10 +9,12 @@ import numpy as np
 
 
 f = lambda x: 0.5 * P_y_4 * u(x)[4]
-pdf_dx = lambda x: 0.0
-pdf_du = lambda x: 0.5 * P_y_4
-pdu_dx = lambda x: dudx(x)[:, 4]
-df_dx = lambda x: pdf_dx(x) + pdf_du(x) * pdu_dx(x)
+# pdf_dx = lambda x: 0.0
+# pdf_du = lambda x: 0.5 * P_y_4
+# pdu_dx = lambda x: dudx(x)[:, 4]
+# df_dx = lambda x: pdf_dx(x) + pdf_du(x) * pdu_dx(x)
+
+df_dx = lambda x: dfdx_adjoint(x)[:, 4]
 
 strategy_functions_dict = DEFAULT_STRATEGY_FUNCTIONS
 strategy_functions_dict['grad_f'] = lambda f, x: df_dx(x)
