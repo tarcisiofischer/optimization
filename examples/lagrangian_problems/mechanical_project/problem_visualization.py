e4ec75f00ad786cb7149c850158fd5a8c5f866dc
Tarcisio Fischer <tarcisio.fischer.cco@gmail.com>
2017-10-05 08:29:00 -0300

    Add a penalized problem builder so that it is possible to visualize the pure penalized problem P

--- examples/lagrangian_problems/mechanical_project/problem_visualization.py
+++ examples/lagrangian_problems/mechanical_project/problem_visualization.py
@@ -1,9 +1,11 @@
 from mpl_toolkits.mplot3d.axes3d import Axes3D
 
 from examples.lagrangian_problems.mechanical_project.constants import h_max, h_min, V_max
-from examples.lagrangian_problems.mechanical_project.constraints import V
-from examples.lagrangian_problems.mechanical_project.linear_system import f
+from examples.lagrangian_problems.mechanical_project.constraints import V, dg7_dx, g7, dg6_dx, g6, \
+    dg5_dx, g5, dg4_dx, g4, dg3_dx, g3, dg2_dx, g2, dg1_dx, g1
+from examples.lagrangian_problems.mechanical_project.linear_system import f, dfdx_adjoint
 from optimization.plotting import FunctionPlotterHelper
+from optimization.utils import build_penalized_function
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -11,8 +13,8 @@ import numpy as np
 h3 = 40.0
 
 # 3D function visualization
-xrange = np.arange(10.0, 80.0, 1.e-1)
-yrange = np.arange(10.0, 80.0, 1.e-1)
+xrange = np.arange(10.0, 80.0, 1.e-0)
+yrange = np.arange(10.0, 80.0, 1.e-0)
 X, Y = np.meshgrid(xrange, yrange)
 F = np.zeros_like(X)
 for ii, xx in enumerate(xrange):
@@ -40,5 +42,38 @@ p.draw_function(broadcastable=False)
 # Show the constrain level curve
 X, Y = np.meshgrid(p._xrange, p._yrange)
 plt.contour(X, Y, V(np.array([X, Y, np.ones_like(X) * h3])), [V_max])
+
+# Penalized problem visualization
+xrange = np.arange(10.0, 80.0, 1.e-0)
+yrange = np.arange(10.0, 80.0, 1.e-0)
+X, Y = np.meshgrid(xrange, yrange)
+F = np.zeros_like(X)
+inequality_constraints = [
+    (g1, dg1_dx, 10.0),  # h1 <= hmax
+    (g2, dg2_dx, 10.0),  # h2 <= hmax
+    (g3, dg3_dx, 10.0),  # h3 <= hmax
+    (g4, dg4_dx, 10.0),  # h1 >= hmin
+    (g5, dg5_dx, 10.0),  # h2 >= hmin
+    (g6, dg6_dx, 10.0),  # h3 >= hmin
+    (g7, dg7_dx, 30000.0),  # h1*b*L + h2*b*L + h3*b*L <= Vmax
+]
+P, grad_P = build_penalized_function(
+    f,
+    3,
+    [],
+    [c[0] for c in inequality_constraints],
+    [c[2] for c in inequality_constraints],
+    dfdx_adjoint,
+    [],
+    [c[1] for c in inequality_constraints],
+)
+for ii, xx in enumerate(xrange):
+    for jj, yy in enumerate(yrange):
+        F[ii][jj] = P(np.array([xx, yy, h3]))
+
+fig = plt.figure()
+ax = Axes3D(fig)
+ax.plot_wireframe(X, Y, F)
+plt.draw()
 
 plt.show()
--- optimization/utils.py
+++ optimization/utils.py
@@ -1,5 +1,53 @@
 import numpy as np
 
+def build_penalized_function(
+    f,
+    n_vars,
+    equality_constraints,
+    inequality_constraints,
+    phi,
+    dfdx,
+    dequality_constraintsdx,
+    dinequality_constraintsdx,
+):
+    n_equality_constraints = len(equality_constraints)
+    n_inequality_constraints = len(inequality_constraints)
+    n_constraints = n_equality_constraints + n_inequality_constraints
+
+    if type(phi) != list:
+        phi = [phi] * n_constraints
+
+    def P(x):
+        result = f(x)
+        for i in range(n_equality_constraints):
+            phi_i = phi[i]
+            h_i = equality_constraints[i](x)
+            result += phi_i * (h_i ** 2)
+        for j in range(n_inequality_constraints):
+            phi_j = phi[n_equality_constraints + j]
+            g_j = inequality_constraints[j](x)
+            g_j_plus = max(0, g_j)
+            result += phi_j * (g_j_plus ** 2)
+
+        return result
+
+    def grad_P(x):
+        dL_dx = dfdx(x)
+        for i in range(n_equality_constraints):
+            phi_i = phi[i]
+            grad_h_i = dequality_constraintsdx[i](x)
+            dL_dx += phi_i * grad_h_i
+        for j in range(n_inequality_constraints):
+            phi_j = phi[n_equality_constraints + j]
+            dg_jdx = dinequality_constraintsdx[j](x)
+            grad_g_j_plus = np.array([max(0.0, dg_jdx_i) for dg_jdx_i in dg_jdx])
+            dL_dx += phi_j * grad_g_j_plus
+
+        return dL_dx
+
+    return P, grad_P
+
+
 def build_augmented_lagrangian(
     f,
     n_vars,
